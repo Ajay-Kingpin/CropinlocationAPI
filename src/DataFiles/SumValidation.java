a8a062638e386fe22c01b19d6a0313fb854fd1f4
Ajay <Ajay.s@Cropin.com>
2024-09-26 19:33:20 +0530

    Updated10thlnSumvalid

--- src/DataFiles/SumValidation.java
+++ src/DataFiles/SumValidation.java
@@ -7,7 +7,7 @@ import io.restassured.path.json.JsonPath;
 
 public class SumValidation {
 	
-	// public void sumofcourses()
+	// public void sumofcourses() check from updating GIT
 	public static void main(String[] args){ 
 	{
 		int sum = 0;
